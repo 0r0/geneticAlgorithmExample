0eb97d2ca330d5d4ad6719cbaeec0a7f8bda7040
mehdi <0r0.developer@gmail.com>
2021-12-13 01:41:29 +0400

    make genetic algorithm reusable as a module

---
+++ genetic.py
@@ -0,0 +1,39 @@
+import random
+
+def _generate_parent(length,genSet):
+    genes = []
+    while len(genes) < length:
+        sampleSize = min(length - len(genes), len(genSet))
+        genes.extend(random.sample(genSet, sampleSize))
+        print('sample size:'.join(list(sampleSize)))
+        print('genes'.join(genes))
+    return ''.join(genes)
+
+
+
+def _mutate(parent: str,genSet):
+    index = random.randrange(0, len(parent))
+    childGenes = list(parent)
+    newGene, alternate = random.sample(genSet, 2)
+    childGenes[index] = alternate if newGene == childGenes[index] else newGene
+    return ''.join(childGenes)
+
+
+def get_best(get_fitness,lenTarget,optimalFitness,display,genSet):
+    random.seed()
+
+    bestParent = _generate_parent(lenTarget,genSet)
+    bestFitness = get_fitness(bestParent)
+    display(bestParent)
+    if bestParent >= optimalFitness:
+        return bestParent
+    while True:
+        child = _mutate(bestParent,genSet)
+        childFitness = get_fitness(child)
+        if bestFitness >= childFitness:
+            continue
+        display(child)
+        if childFitness >= optimalFitness:
+            return child
+        bestFitness = childFitness
+        bestParent = child
