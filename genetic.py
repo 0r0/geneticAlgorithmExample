d46a08c2f5fd0a90f3d1be42aa9111be93da1695
mehdi <0r0.developer@gmail.com>
2021-12-13 14:56:10 +0400

    make genetic module as a class and add benchmark method

--- genetic.py
+++ genetic.py
@@ -1,37 +1,67 @@
 import random
+import statistics
+import sys
+import time
 
-def _generate_parent(length,genSet):
+
+class Chromosome:
+    Genes=None
+    Fitness=None
+    def __init__(self,genes,fitness):
+        self.Genes=genes
+        self.Fitness=fitness
+
+def _generate_parent(length,genSet,get_fitness):
     genes = []
     while len(genes) < length:
         sampleSize = min(length - len(genes), len(genSet))
         genes.extend(random.sample(genSet, sampleSize))
-    return ''.join(genes)
+    fitness=get_fitness(genes)
+    return Chromosome(genes,fitness)
 
 
 
-def _mutate(parent: str,genSet):
-    index = random.randrange(0, len(parent))
-    childGenes = list(parent)
+
+def _mutate(parent,genSet,get_fitness):
+    index = random.randrange(0, len(parent.Genes))
+    childGenes = list(parent.Genes)
     newGene, alternate = random.sample(genSet, 2)
     childGenes[index] = alternate if newGene == childGenes[index] else newGene
-    return ''.join(childGenes)
+    genes=''.join(childGenes)
+    fitness=get_fitness(genes)
+    return Chromosome(genes,fitness)
+
 
 
 def get_best(get_fitness,lenTarget,optimalFitness,display,genSet):
     random.seed()
 
-    bestParent = _generate_parent(lenTarget,genSet)
-    bestFitness = get_fitness(bestParent)
+    bestParent = _generate_parent(lenTarget,genSet,get_fitness)
     display(bestParent)
-    if bestFitness >= optimalFitness:
+    if bestParent.Fitness >= optimalFitness:
         return bestParent
     while True:
-        child = _mutate(bestParent,genSet)
-        childFitness = get_fitness(child)
-        if bestFitness >= childFitness:
+        child = _mutate(bestParent,genSet,get_fitness)
+        if bestParent.Fitness >= child.Fitness:
             continue
         display(child)
-        if childFitness >= optimalFitness:
+        if child.Fitness >= optimalFitness:
             return child
-        bestFitness = childFitness
         bestParent = child
+
+
+class Benchmark:
+    @staticmethod
+    def run(function):
+        timings=[]
+        stdout=sys.stdout
+        for i in range(100):
+            sys.stdout=None
+            startTime=time.time()
+            function()
+            seconds=time.time()-startTime
+            sys.stdout=stdout
+            timings.append(seconds)
+            mean=statistics.mean(timings)
+            if i<10 or i%10==9:
+                print("{0} {1:3.2f}{2:3.2f}".format(1+i,mean,statistics.stdev(timings,mean) if i>1  else 0))
