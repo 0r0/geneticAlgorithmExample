334c7becbeefe8829da45120e4c738b3894ede0b
mehdi <0r0.developer@gmail.com>
2021-12-13 15:04:06 +0400

    move to folder of projects

--- genetic.py
+++ genetic.py
@@ -5,43 +5,42 @@ import time
 
 
 class Chromosome:
-    Genes=None
-    Fitness=None
-    def __init__(self,genes,fitness):
-        self.Genes=genes
-        self.Fitness=fitness
+    Genes = None
+    Fitness = None
 
-def _generate_parent(length,genSet,get_fitness):
+    def __init__(self, genes, fitness):
+        self.Genes = genes
+        self.Fitness = fitness
+
+
+def _generate_parent(length, genSet, get_fitness):
     genes = []
     while len(genes) < length:
         sampleSize = min(length - len(genes), len(genSet))
         genes.extend(random.sample(genSet, sampleSize))
-    fitness=get_fitness(genes)
-    return Chromosome(genes,fitness)
+    fitness = get_fitness(genes)
+    return Chromosome(genes, fitness)
 
 
-
-
-def _mutate(parent,genSet,get_fitness):
+def _mutate(parent, genSet, get_fitness):
     index = random.randrange(0, len(parent.Genes))
     childGenes = list(parent.Genes)
     newGene, alternate = random.sample(genSet, 2)
     childGenes[index] = alternate if newGene == childGenes[index] else newGene
-    genes=''.join(childGenes)
-    fitness=get_fitness(genes)
-    return Chromosome(genes,fitness)
+    genes = ''.join(childGenes)
+    fitness = get_fitness(genes)
+    return Chromosome(genes, fitness)
 
 
-
-def get_best(get_fitness,lenTarget,optimalFitness,display,genSet):
+def get_best(get_fitness, lenTarget, optimalFitness, display, genSet):
     random.seed()
 
-    bestParent = _generate_parent(lenTarget,genSet,get_fitness)
+    bestParent = _generate_parent(lenTarget, genSet, get_fitness)
     display(bestParent)
     if bestParent.Fitness >= optimalFitness:
         return bestParent
     while True:
-        child = _mutate(bestParent,genSet,get_fitness)
+        child = _mutate(bestParent, genSet, get_fitness)
         if bestParent.Fitness >= child.Fitness:
             continue
         display(child)
@@ -53,15 +52,15 @@ def get_best(get_fitness,lenTarget,optimalFitness,display,genSet):
 class Benchmark:
     @staticmethod
     def run(function):
-        timings=[]
-        stdout=sys.stdout
+        timings = []
+        stdout = sys.stdout
         for i in range(100):
-            sys.stdout=None
-            startTime=time.time()
+            sys.stdout = None
+            startTime = time.time()
             function()
-            seconds=time.time()-startTime
-            sys.stdout=stdout
+            seconds = time.time() - startTime
+            sys.stdout = stdout
             timings.append(seconds)
-            mean=statistics.mean(timings)
-            if i<10 or i%10==9:
-                print("{0} {1:3.2f}{2:3.2f}".format(1+i,mean,statistics.stdev(timings,mean) if i>1  else 0))
+            mean = statistics.mean(timings)
+            if i < 10 or i % 10 == 9:
+                print("{0} {1:3.2f}{2:3.2f}".format(1 + i, mean, statistics.stdev(timings, mean) if i > 1 else 0))
